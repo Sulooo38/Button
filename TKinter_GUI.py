2afbc394569a89842d3baff77eb5da30532e63e5
sabes <sabes@tsn.at>
2026-04-28 08:17:49 +0200

    Code fertig mit Sprache änderung

--- TKinter_GUI.py
+++ TKinter_GUI.py
@@ -4,22 +4,36 @@ class Button:
     def __init__(self, root):
         self.root = root
         self.klicks = 0
-        
-        def klick():
-            
-            self.klicks +=1
-            self.label.config(text=f"Button {self.klicks} geklickt")
-            if self.klicks == 1000:
-                print("BOOM!")
+        self.language = "DE"
 
-        self.label = tk.Label(root, text = "Servus!")
+        self.label = tk.Label(root, text="Servus!")
         self.label.pack()
-        
-        self.button = tk.Button(root, text="Klick mich!", command=klick)
+
+        self.button = tk.Button(root, text="Klick mich!", command=self.klick)
         self.button.pack(pady=10)
 
-        
-    
+        self.button1 = tk.Button(root, text="Sprache ändern", command=self.andern)
+        self.button1.pack(pady=10)
+
+    def klick(self):
+        self.klicks += 1
+        self.label.config(text=f"Button {self.klicks} geklickt")
+        if self.klicks == 100:
+            print("BOOM!")
+
+    def andern(self):
+        if self.language == "DE":
+            self.label.config(text="Dil degistirildi")
+            self.button.config(text="Tikla!")
+            self.button1.config(text="Dil degistir")
+            self.language = "TR"
+        else:
+            self.label.config(text="Sprache geändert")
+            self.button.config(text="Klick mich!")
+            self.button1.config(text="Sprache ändern")
+            self.language = "DE"
+
+
 root = tk.Tk()
 root.title("Button")
 gui = Button(root)
